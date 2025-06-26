58cdcfa39a8f9e3277ac99c82016d1a80ce0b84c
edy alves <30.edyalves@gmail.com>
2025-06-26 13:26:35 -0300

    Refactor setupTableView, set set constraints tableView, number of rows, set cell

--- ListViewCode/ToDoListViewController.swift
+++ ListViewCode/ToDoListViewController.swift
@@ -20,12 +20,22 @@ class ToDoListViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "Minhas tarefas"
-        setupView()
+        setupTableView()
     }
     
-    func setupView() {
+    func setupTableView() {
         view.backgroundColor = .white
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
     }
     
     private func saveTasks() {
@@ -36,4 +46,23 @@ class ToDoListViewController: UIViewController {
         tasks = UserDefaults.standard.stringArray(forKey: "tasks") ?? []
     }
     
+}
+
+extension ToDoListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = tasks[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            tasks.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    }
 }
